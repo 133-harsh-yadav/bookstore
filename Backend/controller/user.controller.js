8273b26eb0827e6fee4c0b0c5b55899fef2357a1
Harsh yadav <harshydv1120@gmail.com>
2024-08-18 10:08:15 +0530

    Initial commit

---
+++ Backend/controller/user.controller.js
@@ -0,0 +1,23 @@
+import User from "../model/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+    try {
+        const { fullname, email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+        const hashPassword = await bcryptjs.hash(password, 10);
+        const createdUser = new User({
+            fullname: fullname,
+            email: email,
+            password: hashPassword,
+        });
+        await createdUser.save();
+        res.status(201).json({ message: "User created successfully" });
+    } catch (error) {
+        console.error("Error:", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
